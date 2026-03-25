907731b64fd94c005fb7aad0e563776734f042c5
Nikolay_Kotov_analytics <nakotov88@gmail.com>
2026-03-25 17:23:49 +0100

    Delete data/sql/phase1 directory

--- data/sql/phase1/04_date_range.sql
+++
@@ -1,9 +0,0 @@
--- TASK 4 Assess time range of the dataset
-
-SELECT 
-    MIN(date) as  first_date, -- 2010-01-01 00:01:00
-    MAX(date) as final_date -- 2019-10-31 23:59:00
-FROM transactions_data t;
-
-
- -- Conclusion: Dataset covers total 10 years from start of 2010 till end of 2019.
